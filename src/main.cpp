eec987e47f633218e0bd740a8ded5157f77bb5d9
Alex <alexringme@gmail.com>
2024-01-21 20:58:39 -0500

    Antialiasing

--- src/main.cpp
+++ src/main.cpp
@@ -21,12 +21,15 @@ int main(int argc, char const *argv[])
     world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
     world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
+    //Camera
     camera cam;
 
     cam.aspect_ratio = 16.0 / 9.0;
     cam.image_width = 400;
+    cam.samples_per_pixel = 100;
 
     cam.render(world, output_file, "image.ppm");
    
+
     return 0;
 }
